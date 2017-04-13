8d6d61bac27d54f51616a5b54f86a06227e449a8
gabee1000 <gabee5000@gmail.com>
2017-04-13 21:13:44 +0200

    Sync commit.

--- app/src/main/java/com/example/gabee1000/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gabee1000/myapplication/MainActivity.java
@@ -1,31 +1,39 @@
 package com.example.gabee1000.myapplication;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.gabee1000.myapplication.Database.DBHelper;
+import com.example.gabee1000.myapplication.Database.UserDatabase;
+
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private UserDatabase mDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        init();
+        load();
+    }
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
+    private void init() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDB = new UserDatabase(this);
+    }
+
+    private void load() {
+
     }
 
     @Override
